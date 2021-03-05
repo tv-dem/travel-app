f014cc10a02a9cc7e528f1aacd6eed5c18cc5940
Demchuk Tatyana <69cobak@mail.ru>
2021-03-06 01:51:00 +0300

    init

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const s = require('./Header.modules.css');
+
+const Header: React.FC = () => (
+  <div className={s.header}>
+    header
+  </div>
+)
+export default Header;
