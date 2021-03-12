8e79a483a70f68f4895524dc58aa6cfeab195869
Demchuk Tatyana <69cobak@mail.ru>
2021-03-12 15:03:35 +0300

    feat: fix sizes in gallery

--- src/Pages/MainPage/MainePage.tsx
+++ src/Pages/MainPage/MainePage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
-import Content from '../../Components/Content/Content';
+import Gallery from '../../Components/Gallery/Gallery';
 
 const MainPage: React.FC = () => (<>
     <Header />
-    <Content />
+    <Gallery/>
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad aliquam aperiam asperiores, ex iusto laudantium maxime necessitatibus odit quidem. Cum delectus libero nesciunt officiis placeat quia vitae. Quasi, quidem.
+    </div>
     <Footer />
   </>
 );
