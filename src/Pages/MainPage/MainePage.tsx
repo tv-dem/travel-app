881fbf484f820aaa26d48bdf5507097c2d5f75cf
Demchuk Tatyana <69cobak@mail.ru>
2021-03-13 18:53:03 +0300

    feat: add video component

--- src/Pages/MainPage/MainePage.tsx
+++ src/Pages/MainPage/MainePage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
-import Content from '../../Components/Content/Content';
+import Video from '../../Components/Video/Video';
 
 const MainPage: React.FC = () => (<>
     <Header />
-    <Content />
+    <Video />
     <Footer />
   </>
 );
