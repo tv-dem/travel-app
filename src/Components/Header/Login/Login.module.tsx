60f2505bb907dc282308cb82fe39a6a56e5c4716
kli2m <ivanklimm87@gmail.com>
2021-03-15 15:53:34 +0300

    refactor: fix Logout & Show Err

--- src/Components/Header/Login/Login.module.tsx
+++ src/Components/Header/Login/Login.module.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
@@ -9,25 +9,30 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       '& > *': {
         margin: theme.spacing(1),
-      }   
+      },
     },
-    paper_login:{
-      margin: '10px 0 0',
+    paper_login: {
+      margin:"10px 0",
+      padding: '20px',
       alignItems: 'center',
-      width: '100%',
-    }
+    },
   }),
 );
 
-
 const Login: React.FC = () => {
   const styles = useStyles();
-  return  ( <Paper className={styles.paper_login}>
-    <div className={styles.root}>
-    <Link to="/authorization"><Button variant="contained">Sign in</Button></Link>
-    <Link to="/reset"><Button variant="contained">Sign up</Button></Link>
-    </div>
-    </Paper>)
+  return (
+    <Paper className={styles.paper_login}>
+      <div className={styles.root}>
+        <Link to="/authorization">
+          <Button variant="contained">Sign in</Button>
+        </Link>
+        <Link to="/reset">
+          <Button variant="contained">Sign up</Button>
+        </Link>
+      </div>
+    </Paper>
+  );
 };
 
 export default Login;
